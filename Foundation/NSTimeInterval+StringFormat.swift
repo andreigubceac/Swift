9b7b8565659473d07ec3cf295cf999cd7c8543da
Andrei Gubceac <andrei.gubceac@geminisols.com>
2016-03-18 16:22:59 +0200

    TimeInterfac string format

---
+++ Foundation/NSTimeInterval+StringFormat.swift
@@ -0,0 +1,29 @@
+//
+//  NSTimeInterval+StringFormat.swift
+//
+//  Created by Andrei Gubceac on 3/18/16.
+//  Copyright © 2016 Andrei Gubceac. All rights reserved.
+//
+
+extension NSTimeInterval {
+    
+    func formatHrMin() -> String {
+        var mm = Int(self / 60)
+        let hh = Int(mm / 60)
+        if hh > 0 {
+            mm %= 60
+        }
+        return ("" + (hh > 0 ?"\(hh) hr" + (hh == 1 ? "" : "s") : "") + (mm > 0 ? " \(mm) min" : "")).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
+
+    func formatHrMinSec() -> String {
+        let ss = self % 60
+        var mm = Int(self / 60)
+        let hh = Int(mm / 60)
+        if hh > 0 {
+            mm %= 60
+        }
+        return ("" + (hh > 0 ? "\(hh) hr" + (hh > 1 ? "s" : "") : "") + (mm > 0 ? " \(mm) min" : "") + " \(ss) sec").stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
+
+}
