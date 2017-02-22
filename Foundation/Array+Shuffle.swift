2a09385a76ad60cbb073c2b894e4e45cfda7fe1b
Andrei Gubceac <andrei.gubceac@me.com>
2017-02-22 20:05:48 +0200

    [Array] Shuffle

---
+++ Foundation/Array+Shuffle.swift
@@ -0,0 +1,18 @@
+//
+//  Array+Shuffle.swift
+//
+//
+
+import Foundation
+
+extension Array {
+    mutating func shuffle() {
+        if count < 2 { return }
+        for i in 0..<(count - 1) {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            if i != j {
+                swap(&self[i], &self[j])
+            }
+        }
+    }
+}
