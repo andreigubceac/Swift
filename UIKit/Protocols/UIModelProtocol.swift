4407ce9bc53c0f46571ab607a82095fcb12ae6bf
Andrei Gubceac <andrei.gubceac@me.com>
2018-10-21 11:24:36 +0300

    New UI protocol

---
+++ UIKit/Protocols/UIModelProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  UIModelProtocol.swift
+//  Pickapal
+//
+//  Created by Andrei Gubceac on 20/10/2018.
+//  Copyright © 2018 Pickapal Inc. All rights reserved.
+//
+
+import Foundation
+
+protocol UIModelProtocol {
+    var title : String { get }
+    var subtitle : String? { get }
+}
