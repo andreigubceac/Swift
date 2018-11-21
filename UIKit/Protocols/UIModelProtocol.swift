8e7103e1c6492eaacd9c31b3d2e47f97ff3a0a34
Andrei Gubceac <andrei.gubceac@geminisols.com>
2018-11-21 20:00:47 +0200

    Remove project name

--- UIKit/Protocols/UIModelProtocol.swift
+++ UIKit/Protocols/UIModelProtocol.swift
@@ -1,9 +1,8 @@
 //
 //  UIModelProtocol.swift
-//  Pickapal
 //
 //  Created by Andrei Gubceac on 20/10/2018.
-//  Copyright © 2018 Pickapal Inc. All rights reserved.
+//  Copyright © 2018. All rights reserved.
 //
 
 import Foundation
