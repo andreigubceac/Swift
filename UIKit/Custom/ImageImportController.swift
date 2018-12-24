33b35d720bd14d1a4929346f34a1e131490e5ed3
Andrei Gubceac <andrei.gubceac@me.com>
2018-12-24 16:29:23 +0200

    clean up

--- UIKit/Custom/ImageImportController.swift
+++ UIKit/Custom/ImageImportController.swift
@@ -113,26 +113,14 @@ extension UIViewController: UIImagePickerControllerDelegate, UINavigationControl
     }
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-// Local variable inserted by Swift 4.2 migrator.
-let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
-
+        // Local variable inserted by Swift 4.2 migrator.
         picker.dismiss(animated: true) {
             if let pickerVctrl = picker as? ImagePickerController {
-                if let image = (info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] ?? info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)]) as? UIImage {
+                if let image = (info[UIImagePickerController.InfoKey.editedImage] ?? info[UIImagePickerController.InfoKey.originalImage]) as? UIImage {
                     pickerVctrl.delegateBlock?(image, picker.sourceType)
                 }
             }
         }
     }
     
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
 }
