b3a931bab604c98e245d23b26b4a99838c4d5ca5
Andrei Gubceac <andrei.gubceac@me.com>
2019-07-16 22:57:27 +0300

    remove default implementation for Encodable

--- Foundation/ApplicationModel+Analytics.swift
+++ Foundation/ApplicationModel+Analytics.swift
@@ -16,11 +16,3 @@ extension ApplicationModel : AGAnalyticsEventParameters {
     }
 }
 
-extension Encodable {
-
-    var eventParameters: [String: Any]? {
-        guard let data = try? JSONEncoder().encode(self) else { return nil }
-        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }
-    }
-}
-
