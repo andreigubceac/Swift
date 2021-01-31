7f542b6a5454f0fd33e67e5bdf03b09514cb3a05
Andrei Gubceac <andrei.gubceac@me.com>
2021-01-31 21:08:38 +0200

    New image resize method

--- UIKit/Extensions/UIImage+Graphics.swift
+++ UIKit/Extensions/UIImage+Graphics.swift
@@ -17,3 +17,11 @@ extension UIImage {
         return image!
     }
 }
+
+extension UIImage {
+    func imageResized(to size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
